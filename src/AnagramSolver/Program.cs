a754d41a0c866b88a7b8c584012fdea6286bd93f
Ivan Vukman <ivan.vukman@gmail.com>
2023-08-22 00:50:22 +0200

    Use correct value for connection string

--- src/AnagramSolver/Program.cs
+++ src/AnagramSolver/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AnagramSolverContext>(options =>
-            options.UseNpgsql(builder.Configuration.GetValue<string>("DATABASE_URL")));
+            options.UseNpgsql(builder.Configuration.GetValue<string>("CONNECTION_STRING")));
 
 var app = builder.Build();
 
