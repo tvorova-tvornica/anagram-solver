2eae6d2648c7bb04c83f74c59b0363412006a571
Gabriel Nakic <gnakic@proton.me>
2023-08-22 01:48:33 +0200

    initial setup

---
+++ src/AnagramSolver/ClientApp/src/components/anagram-solver/AnagramSolver.tsx
@@ -0,0 +1,10 @@
+import { Box, Input } from "@chakra-ui/react";
+import { FC } from "react";
+
+export const AnagramSolver: FC<{}> = () => {
+    return (
+        <Box minW="600px">
+            <Input placeholder="..." />
+        </Box>
+    );
+};
