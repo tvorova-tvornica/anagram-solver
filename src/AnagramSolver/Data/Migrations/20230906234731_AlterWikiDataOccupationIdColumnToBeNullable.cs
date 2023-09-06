e94be67208248963a8c6a6adbdeb6fe8fa97b19a
Ivan Vukman <ivan.vukman@gmail.com>
2023-09-07 01:47:55 +0200

    Add migration to change occupation id nullability

---
+++ src/AnagramSolver/Data/Migrations/20230906234731_AlterWikiDataOccupationIdColumnToBeNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnagramSolver.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterWikiDataOccupationIdColumnToBeNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WikiDataOccupationId",
+                table: "ImportWikiDataCelebritiesRequests",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WikiDataOccupationId",
+                table: "ImportWikiDataCelebritiesRequests",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
