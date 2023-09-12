df9b784af32ede62160772f087dca14242568ad3
Gabriel Nakic <gnakic@proton.me>
2023-09-12 23:09:44 +0200

    marginal cleanup

--- src/AnagramSolver/Program.cs
+++ src/AnagramSolver/Program.cs
@@ -18,38 +18,31 @@ var builder = WebApplication.CreateBuilder(args);
 
 if (sentryDSN is not null)
 {
+    // This option is recommended. It enables Sentry's "Release Health" feature.
     var autoSessionTracking = true;
+    // Enabling this option is recommended for client applications only. It ensures all threads use the same global scope.
     var isGlobalModeEnabled = false;
+    // This option will enable Sentry's tracing features. You still need to start transactions and spans.
     var enableTracing = true;
+    // Example sample rate for your transactions: captures 10% of transactions
     var tracesSampleRate = 0.1;
 
     SentrySdk.Init(options =>
     {
         options.Dsn = sentryDSN;
-
-        // This option is recommended. It enables Sentry's "Release Health" feature.
         options.AutoSessionTracking = autoSessionTracking;
-
-        // Enabling this option is recommended for client applications only. It ensures all threads use the same global scope.
         options.IsGlobalModeEnabled = isGlobalModeEnabled;
-
-        // This option will enable Sentry's tracing features. You still need to start transactions and spans.
         options.EnableTracing = enableTracing;
-
-        // Example sample rate for your transactions: captures 10% of transactions
         options.TracesSampleRate = tracesSampleRate;
     });
 
-    builder.Host.ConfigureLogging(c =>
+    builder.Logging.AddSentry(options =>
     {
-        c.AddSentry(options =>
-        {
-            options.Dsn = sentryDSN;
-            options.AutoSessionTracking = autoSessionTracking;
-            options.IsGlobalModeEnabled = isGlobalModeEnabled;
-            options.EnableTracing = enableTracing;
-            options.TracesSampleRate = tracesSampleRate;
-        });
+        options.Dsn = sentryDSN;
+        options.AutoSessionTracking = autoSessionTracking;
+        options.IsGlobalModeEnabled = isGlobalModeEnabled;
+        options.EnableTracing = enableTracing;
+        options.TracesSampleRate = tracesSampleRate;
     });
 }
 
