e95d207222bd903891ce15ff2f73d35965b786ad
Ivan Vukman <ivan.vukman@gmail.com>
2023-09-15 10:24:17 +0200

    Make Anagram required

--- src/AnagramSolver/Controllers/Dto/ResolveAnagramDto.cs
+++ src/AnagramSolver/Controllers/Dto/ResolveAnagramDto.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using AnagramSolver.Extensions;
 
 namespace AnagramSolver.Controllers.Dto;
 
 public record ResolveAnagramDto
 {
+    [Required]
     public required string Anagram { get; init; }
-    public string AnagramKey => Anagram.ToAnagramKey();
+    public string? AnagramKey => Anagram?.ToAnagramKey();
 }
