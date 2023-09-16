c764c0b6025b8d71d111da32fc8931357557f0a8
Ivan Vukman <ivan.vukman@gmail.com>
2023-09-17 01:41:50 +0200

    Remove unused import

--- src/AnagramSolver/ClientApp/src/index.tsx
+++ src/AnagramSolver/ClientApp/src/index.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { theme } from "./theme";
 import { ToggleColorMode } from "./components/toggle-color-mode";
