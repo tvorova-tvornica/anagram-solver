e5ea1c6d8b23183070633d9cefa3f1bc9f55277c
Ivan Vukman <ivan.vukman@gmail.com>
2023-09-19 00:47:39 +0200

    Clean up job logic

--- src/AnagramSolver/BackgroundJobs/WikiDataImport/ScheduleCelebrityPagesImportJob.cs
+++ src/AnagramSolver/BackgroundJobs/WikiDataImport/ScheduleCelebrityPagesImportJob.cs
@@ -1,6 +1,7 @@
 using AnagramSolver.Data;
 using AnagramSolver.HttpClients;
 using Microsoft.EntityFrameworkCore;
+using static AnagramSolver.Data.Entities.ImportWikiDataCelebritiesRequest;
 
 namespace AnagramSolver.BackgroundJobs.WikiDataImport;
 
@@ -17,7 +18,13 @@ namespace AnagramSolver.BackgroundJobs.WikiDataImport;
 
     public async Task ScheduleAsync(int importCelebritiesRequestId)
     {
-        var request = await _db.ImportWikiDataCelebritiesRequests.SingleAsync(x => x.Id == importCelebritiesRequestId);
+        var request = await _db.ImportWikiDataCelebritiesRequests.SingleOrDefaultAsync(x => x.Id == importCelebritiesRequestId && 
+                                                                                            x.Status == ImportWikiDataCelebritiesRequestStatus.Scheduled);
+
+        if (request is null)
+        {
+            throw new Exception($"Cannot find import request (requestId: {importCelebritiesRequestId}) with status 'Scheduled'");
+        }
 
         var totalCelebrityCount = await _httpClient.GetTotalCelebrityCountAsync(request.WikiDataOccupationId, request.WikiDataNationalityId);
 
