b3a1cb296878d82bd885824a91408ed86cf8f908
Ivan Vukman <ivan.vukman@gmail.com>
2023-09-21 17:24:29 +0200

    Clean up sentry performance monitoring config

--- src/AnagramSolver/Program.cs
+++ src/AnagramSolver/Program.cs
@@ -146,19 +146,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.Use(async (context, next) => {
-    var anagramQueryParam = context.Request.Query["anagram"].ToString();
-
-    if (string.IsNullOrWhiteSpace(anagramQueryParam))
-    {
-        await next.Invoke();
-    }
-    else
-    {
-        var transaction = SentrySdk.StartTransaction("celebrity-anagram", "resolve-anagram");
-        SentrySdk.ConfigureScope(scope => scope.Transaction = transaction);
-        await next.Invoke();
-        transaction.Finish();
-    }
+    var transaction = SentrySdk.StartTransaction(context.Request.Path, context.Request.Method);
+    SentrySdk.ConfigureScope(scope => scope.Transaction = transaction);
+    await next.Invoke();
+    transaction.Finish();
 });
 
 GlobalConfiguration.Configuration
