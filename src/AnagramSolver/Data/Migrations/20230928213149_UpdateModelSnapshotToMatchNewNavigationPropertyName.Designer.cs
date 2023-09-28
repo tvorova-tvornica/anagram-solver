4e12b841b4246002fc9e4ae30385aa635a35bf54
Ivan Vukman <ivan.vukman@gmail.com>
2023-09-28 23:32:40 +0200

    Update model snapshot

--- src/AnagramSolver/Data/Migrations/20230928213149_UpdateModelSnapshotToMatchNewNavigationPropertyName.Designer.cs
+++ src/AnagramSolver/Data/Migrations/20230928213149_UpdateModelSnapshotToMatchNewNavigationPropertyName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AnagramSolver.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AnagramSolver.Data.Migrations
 {
     [DbContext(typeof(AnagramSolverContext))]
-    partial class AnagramSolverContextModelSnapshot : ModelSnapshot
+    [Migration("20230928213149_UpdateModelSnapshotToMatchNewNavigationPropertyName")]
+    partial class UpdateModelSnapshotToMatchNewNavigationPropertyName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
